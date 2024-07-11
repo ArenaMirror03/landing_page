a427953f9342c9fd8ca70c8b0bcb611ba9514ddb
ArenaMirror03 <officialsumitsharma03@gmail.com>
2024-07-11 12:56:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".container .letter-t", {
+    duration: 1000,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".container img", {
+    duration: 1000,
+    delay: 1500,
+});
+
+
+ScrollReveal().reveal(".container .text_left", {
+    ...scrollRevealOption,
+    origin: "right",
+    delay: 2000,
+});
+ScrollReveal().reveal(".container .text_right", {
+    ...scrollRevealOption,
+    origin: "left",
+    delay: 2000,
+});
+
+ScrollReveal().reveal(".container .explore", {
+    duration: 1000,
+    delay: 2500,
+});
+ScrollReveal().reveal(".container .catalog", {
+    duration: 1000,
+    delay: 2500,
+});
+ScrollReveal().reveal(".container .print", {
+    duration: 1000,
+    delay: 2500,
+});
+
+function internship(){
+    window.open("httphttps://docs.google.com/forms/d/e/1FAIpQLSdddr_JaCY6zRFfm2dVMDm2tFmW1q3l-Oo5S2zoNeUKDV_CRw/viewforms://www.technohacks.co.in/")
+}
+
+function joinus(){
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSdddr_JaCY6zRFfm2dVMDm2tFmW1q3l-Oo5S2zoNeUKDV_CRw/viewform")
+}
+
+function connect(){
+    window.open("https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Aorganization%3A90531650&keywords=technohacks%20edutech%20official&origin=RICH_QUERY_SUGGESTION&position=0&searchId=db28d4ab-53b1-493c-95a4-01c0e94048f9&sid=xm8&spellCorrectionEnabled=false")
+}
+function enquirenow (){
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSdymMOMs6Txpf2fyXWAoh57BHhURY-nMEOCAO6Zm1G-QXuRFA/viewform")
+}
